49be5d15cee34f1f1219abb7d7d46d4e87b92b24
Raphael Coelho <raphaelcoel@gmail.com>
2023-11-06 18:09:02 -0300

    add addCorsMappings

--- src/main/java/com/coelho/brasileiro/expensetrack/config/WebMvcConfig.java
+++ src/main/java/com/coelho/brasileiro/expensetrack/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.coelho.brasileiro.expensetrack.config;
 
 import com.coelho.brasileiro.expensetrack.handle.CustomInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 //       registry.addInterceptor(new CustomInterceptor());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*");
     }
 }

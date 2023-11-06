b19bfd519425c0a7967083de74c943baf1f58e78
Raphael Coelho <raphaelcoel@gmail.com>
2023-11-06 19:08:35 -0300

    add addCorsMappings

--- src/main/java/com/coelho/brasileiro/expensetrack/config/SecurityConfiguration.java
+++ src/main/java/com/coelho/brasileiro/expensetrack/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ package com.coelho.brasileiro.expensetrack.config;
 import com.coelho.brasileiro.expensetrack.filter.FilterChainExceptionHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,7 +20,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.addFilterBefore(filterChainExceptionHandler, LogoutFilter.class)
-                .csrf().disable();
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/v1/categories").permitAll()
+                .and().cors().and().csrf().disable();
     }
-
 }
